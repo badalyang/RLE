4048698a3ed93f856d8186e664eb98819bc4ac03
badalyang <126382208+badalyang@users.noreply.github.com>
2024-10-29 15:13:22 +0400

    Update RLE1.cpp

--- RLE1.cpp
+++ RLE1.cpp
@@ -1,32 +1,48 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 class RLE {
 private:
     string input;
-    
+
 public:
     RLE(const string& str) : input(str) {}
-    
+
     string encode() {
         string encoded = "";
         int n = input.length();
-        
+
         for (int i = 0; i < n; i++) {
             int count = 1;
+
             while (i < n - 1 && input[i] == input[i + 1]) {
                 count++;
                 i++;
             }
-            
+
             encoded += input[i];
-            encoded += to_string(count);
+            encoded += static_cast<char>(count + '0');
         }
-        
+
         return encoded;
     }
-    
+
+    string decode(const string& encoded) {
+        string decoded = "";
+        int n = encoded.length();
+
+        for (int i = 0; i < n; i++) {
+            char currentChar = encoded[i];
+            i++;
+            int count = encoded[i] - '0';
+            decoded.append(count, currentChar);
+        }
+
+        return decoded;
+    }
+
     string getInput() const {
         return input;
     }
@@ -41,5 +57,10 @@ int main() {
     string encodedString = rle.encode();
     cout << "Encoded string: " << encodedString << endl;
 
+    string decodedString = rle.decode(encodedString);
+    cout << "Decoded string: " << decodedString << endl;
+
     return 0;
 }
+
+
